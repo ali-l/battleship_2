4accc30b6f6fb98621d838ad422416066c795034
Alishan Ladhani <alishan.ladhani@gmail.com>
2020-02-01 16:36:05 -0500

    Ignore hits when guessing

--- backend/Random.ts
+++ backend/Random.ts
@@ -3,7 +3,7 @@ import Square, { Status } from "./Square"
 
 export function guess(grid: Grid): number {
   return grid.squares
-    .filter(square => square.status != Status.miss)
+    .filter(square => square.status != Status.miss && square.status != Status.hit)
     .run(squares => pickIndex(squares))
 }
 
@@ -15,9 +15,16 @@ function pickIndex(squares: Array<Square>): number {
 declare global {
   interface Array<T> {
     run<T, R>(this: Array<T>, block: (arg: Array<T>) => R): R
+
+    also<T>(this: Array<T>, block: (arg: Array<T>) => void): Array<T>
   }
 }
 
 Array.prototype.run = function run<T, R>(this: Array<T>, block: (arg: Array<T>) => R): R {
   return block(this)
+}
+
+Array.prototype.also = function also<T>(this: Array<T>, block: (arg: Array<T>) => void): Array<T> {
+  block(this)
+  return this
 }
